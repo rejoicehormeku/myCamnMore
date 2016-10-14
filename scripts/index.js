79c8b4101a1dc3024307c08d78b47b120f478ffe
Rhegyois <rejoicehormeku>
2016-10-14 17:26:30 +0000

    new commits--added barcode reader

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,13 @@
 	}
 
 })();
+
+(function barcode(){
+	
+cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+})();
