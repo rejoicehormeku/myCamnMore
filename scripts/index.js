ff5484f716569f6d236df0a9dc194cec9f22392b
Rhegyois <rejoicehormeku>
2016-10-14 18:06:30 +0000

    new changes to barcode and camera

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-﻿(function() {
-
+﻿	(function()	{
+		
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource;
 	var destinationType;
@@ -34,14 +34,30 @@
 
 	}
 
-})();
-
-(function barcode(){
+	document.getElementById("barcode").onclick = function barcode(){
 	
-cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-        );
-})();
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          "preferFrontCamera" : true, // iOS and Android 
+          "showFlipCameraButton" : true, // iOS and Android 
+          "prompt" : "Place a barcode inside the scan area", // supported on Android only 
+          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
+      }
+   );
+	};
+	
+	
+	})();
+	
+
+
